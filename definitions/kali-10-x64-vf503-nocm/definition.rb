fa9f4da03864153bbdd4486dac269093c4fff1f2
Ivan Leichtling <ivanlei@yelp.com>
2013-07-16 10:54:38 -0700

    Kali Linux 1.0.3 VeeWee template for building Vagrant boxes

---
+++ definitions/kali-10-x64-vf503-nocm/definition.rb
@@ -0,0 +1,51 @@
+#Thanks to Fletcher Nichol - https://github.com/fnichol
+
+Veewee::Definition.declare({
+  :cpu_count => '1',
+  :memory_size=> '1024',
+  :disk_size => '20480', :disk_format => 'VDI', :hostiocache => 'off',
+  :os_type_id => 'Debian_64',
+  :iso_file => "kali-linux-1.0.3-amd64.iso",
+  :iso_src => "http://archive-7.kali.org/kali-images/kali-linux-1.0.3-amd64.iso",
+  :iso_md5 => "95a0eab94407d7ebf0ec6fbd189d883aa772d21d",
+  :iso_download_timeout => "1000",
+  :boot_wait => "10", :boot_cmd_sequence => [
+     '<Esc>',
+     'install ',
+     'url=http://%IP%:%PORT%/preseed.cfg ',
+     'noconfig=sudo ',
+     'username=root ',
+     'locale=en_US.UTF-8 ',
+     'kbd=us ',
+     'keymap=us ',
+     'hostname=%NAME% ',
+     'domain=vagrantup.com ',
+     'auto=true ',
+     'noprompt ',
+     'priority=critical ',
+     'debconf/frontend=noninteractive ',
+     'console-setup/ask_detect=false ',
+     'console-keymaps-at/keymap=us ',
+     '<Enter>'     
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => "10000",
+  :kickstart_file => "preseed.cfg",
+  :ssh_login_timeout => "10000",
+  :ssh_user => "vagrant",
+  :ssh_password => "vagrant",
+  :ssh_key => "",
+  :ssh_host_port => "7222",
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S bash '%f'",
+  :shutdown_cmd => "halt -p",
+  :postinstall_files => [
+    "base.sh",
+    "vagrant.sh",
+    "vmfusion.sh",
+    "postinstall.sh",
+    "cleanup.sh",
+    "zerodisk.sh"
+  ],
+  :postinstall_timeout => "10000"
+})
---
+++ definitions/kali-10-x64-vf503-nocm/postinstall.sh
@@ -0,0 +1,3 @@
+#apt-get -y install git
+#apt-get -y install lubuntu-desktop
+#apt-get -y install emacs
